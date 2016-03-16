75d6706b7ebbe3821fccee315c6db078a0adc2b4
Jon Freed <jon-freed@users.noreply.github.com>
2016-03-16 12:45:49 -0400

    Create WrapWithIndent.js
    skeleton

---
+++ WrapWithIndent.js
@@ -0,0 +1,15 @@
+// WrapWithIndent.js
+//
+// JavaScript for wrapping plain text to a fixed length and also indent based 
+// on the first character(s) in each line.
+//
+// Incidentally, we're Unabashed fan of Hungarian-like notation around here.  
+// (Harumph right back at 'cha. )
+//
+function sWrapWithIndent( sIn, iMaxWidth /*, bBreakLongerTokens*/ ) {
+
+  var sOut;
+  
+  
+  return sOut;
+}
